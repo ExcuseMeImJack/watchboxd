fa54835c28d381d1c9c568eebc3d10ce4163cdbd
Jack Roybal <excusemeimjack@gmail.com>
2023-05-17 08:46:49 -0700

    Added profile img to db

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,17 +5,17 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        first_name='Demo', last_name='User', username='Demo', email='demo@aa.io', password='password')
+        first_name='Demo', last_name='User', username='Demo', email='demo@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
     marnie = User(
-        first_name='Marnie', last_name='Gates',username='marnie', email='marnie@aa.io', password='password')
+        first_name='Marnie', last_name='Gates',username='marnie', email='marnie@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
     bobbie = User(
-        first_name='Bobbie', last_name='Salazar',username='bobbie', email='bobbie@aa.io', password='password')
+        first_name='Bobbie', last_name='Salazar',username='bobbie', email='bobbie@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
     tester1 = User(
-        first_name='Jack', last_name='Roybal',username='tester1', email='tester1@aa.io', password='password')
+        first_name='Jack', last_name='Roybal',username='tester1', email='tester1@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
     tester2 = User(
-        first_name='Andre', last_name='Hristu',username='tester2', email='tester2@aa.io', password='password')
+        first_name='Andre', last_name='Hristu',username='tester2', email='tester2@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
     tester3 = User(
-        first_name='David', last_name='Kim',username='tester3', email='tester3@aa.io', password='password')
+        first_name='David', last_name='Kim',username='tester3', email='tester3@aa.io', profile_img_url='https://d3ki9tyy5l5ruj.cloudfront.net/obj/3d4665c7cf119dc9dc38232301b18fa68b9bb17c/avatar.svg', password='password')
 
     db.session.add(demo)
     db.session.add(marnie)
