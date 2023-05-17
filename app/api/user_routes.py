3ca562940763a54f47407bc26c6158d19782a4dd
Jack Roybal <excusemeimjack@gmail.com>
2023-05-17 08:40:40 -0700

    Added edit user WIP

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
 from app.models import User
 
 user_routes = Blueprint('users', __name__)
@@ -14,12 +14,24 @@ def users():
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
 
-
 @user_routes.route('/<int:id>')
 @login_required
-def user(id):
+def user_by_id(id):
     """
     Query for a user by id and returns that user in a dictionary
     """
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/', methods=['PUT'])
+@login_required
+def update_user():
+    """
+    Update the current user and returns that user in a dictionary
+    """
+    curr_user = User.query.get(current_user.id)
+    form = editUserForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        profile_pic = form.data["user"]
+        if profile_pic
