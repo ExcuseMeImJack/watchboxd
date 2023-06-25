ff7cf918fd973b14c4b4b536a7bb1e9eb6fcb90f
Jack Roybal <excusemeimjack@gmail.com>
2023-06-25 11:22:08 -0700

    Added Custom Histogram to Review Display

--- react-app/src/components/ReviewsHistogram/index.js
+++ react-app/src/components/ReviewsHistogram/index.js
@@ -1,38 +1,46 @@
-import './ReviewsHistogram.css'
+import "./ReviewsHistogram.css";
 
-const ReviewsHistogram = ({reviewCounter}) => {
-
+const ReviewsHistogram = ({ reviewCounter }) => {
   const highestRating = () => {
     let highest = 0;
-    for(let i = 0; i < reviewCounter.length; i++){
-      if(Object.values(reviewCounter[i])[0] > highest){
+    for (let i = 0; i < reviewCounter.length; i++) {
+      if (Object.values(reviewCounter[i])[0] > highest) {
         highest = Object.values(reviewCounter[i])[0];
       }
     }
     return highest;
-  }
+  };
 
   const calculatedHeight = (ratingCount) => {
     let height;
-    if(Object.values(ratingCount) / highestRating() === 0){
+    if (Object.values(ratingCount) / highestRating() === 0) {
       height = 0.05 * 40;
     } else {
-      height = ((Object.values(ratingCount) / highestRating()) * 40)
+      height = (Object.values(ratingCount) / highestRating()) * 40;
     }
-    return {height: height}
-  }
+    return { height: height };
+  };
 
-  return(
-    <div className='histogram-panel'>
+  return (
+    <div className="histogram-panel">
       {reviewCounter.map((ratingCount, i) => (
-        <div id='histogram-bar'
-        key={i}
-        style={calculatedHeight(ratingCount)}
-        title={`${Object.keys(ratingCount)[0] === "1" ? Object.keys(ratingCount) + ' Star' : Object.keys(ratingCount) + ' Stars'}  | ${Object.values(ratingCount)} Ratings`}>
-        </div>
-  ))}
+        <div
+          id="histogram-bar"
+          key={i}
+          style={calculatedHeight(ratingCount)}
+          title={`${
+            Object.keys(ratingCount)[0] === "1"
+              ? Object.keys(ratingCount) + " Star"
+              : Object.keys(ratingCount) + " Stars"
+          } | ${
+            Object.values(ratingCount)[0] === 1
+              ? Object.values(ratingCount) + " Rating"
+              : Object.values(ratingCount) + " Ratings"
+          }`}
+        ></div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ReviewsHistogram
+export default ReviewsHistogram;
