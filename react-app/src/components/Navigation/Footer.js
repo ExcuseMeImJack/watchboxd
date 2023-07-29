30c16549b7307dfed54ece0443f186c3feb6aaf0
Jack Roybal <excusemeimjack@gmail.com>
2023-07-29 13:56:27 -0700

    Home Page restyled with Bootstrap

--- react-app/src/components/Navigation/Footer.js
+++ react-app/src/components/Navigation/Footer.js
@@ -4,14 +4,14 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="social-links">
+    <footer className="footer container-fluid text-center">
+      <div className="social-links col">
         <a href="https://github.com/ExcuseMeImJack">
           <i className="fa-brands fa-github"></i> ExcuseMeImJack
         </a>
       </div>
 
-      <div className="social-links">
+      <div className="social-links col">
         <a href="https://www.linkedin.com/in/jack-roybal-719909264/">
           <i className="fa-brands fa-linkedin"></i> Jack Roybal
         </a>
